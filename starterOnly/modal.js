883d1046f1954f24b91036043c057882fa3c6e94
MacimeG <maxime.glaneux@gmail.com>
2022-04-14 12:03:33 +0200

    ajout d'écouteur d'evenement, pour pouvoir fermer la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,5 +19,12 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+// ici je vais recuperer la classe qui vas me servir a fermer la modal
 
+const btnClose = document.querySelector(".close");
 
+// ici je vais mettre en place mon ecouteur d'evenement qui vas me permettre au clique, de fermer la modal. Je ferme la modal en recuperant la ligne de code qui permet de l'ouvrir, je remplace block par none, pour qu'il disparaisse.
+
+btnClose.addEventListener("click", function(){
+  modalbg.style.display = "none";
+})
